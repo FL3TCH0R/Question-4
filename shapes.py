4a352ba2ea5674206348f12a29a361df81eeffc4
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2023-10-06 13:12:49 -0400

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,18 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> r=6
+>>> h=9
+>>> a=2
+>>> circle=(math.pi)*r*r
+>>> cube=6*a*a
+>>> print(circle)
+113.09733552923255
+>>> print(cube)
+24
+>>> cylinder=2*(math.pi)*r*h+2*(math.pi)*r*r
+>>> print(cylinder)
+565.4866776461628
+>>> sphere=4*(math.pi)*r*r
+>>> print(sphere)
+452.3893421169302
